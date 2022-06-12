35b3a8e4f951872a4911e64e65a08d9d234b58e6
EgorAn <57406043+Egor148@users.noreply.github.com>
2022-06-12 15:36:56 +0700

    добавлен метод для получения курса валют

--- kurs_valut/Form1.cs
+++ kurs_valut/Form1.cs
@@ -15,11 +15,33 @@ namespace kurs_valut
         public Form1()
         {
             InitializeComponent();
+            double c1 = 0, c2;
+            //объект класса Valute
+            Valute val = new Valute();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
            
+        }
+        public string get_kurs(string name)
+        {
+
+            string url = "http://www.cbr.ru/scripts/XML_daily.asp";
+            //string url = "https://tursportopt.ru/category/rybolovnye-tovary-optom/";
+            //XmlDocument xml_doc = new XmlDocument();
+            // xml_doc.Load(url);
+            DataSet ds = new DataSet();
+            ds.ReadXml(url);
+            DataTable currency = ds.Tables["Valute"];
+            foreach (DataRow row in currency.Rows)
+            {
+                if (row["CharCode"].ToString() == name)//Ищу нужный код валюты
+                {
+                    return row["Value"].ToString(); //Возвращаю значение курсы валюты
+                }
+            }
+            return "";
         }
     }
 }
