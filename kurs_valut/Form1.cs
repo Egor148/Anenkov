e6825acb747761767c7cdafb13bbc5d5bbf86e9b
EgorAn <57406043+Egor148@users.noreply.github.com>
2022-06-12 16:00:44 +0700

    метод для отношения курсов

--- kurs_valut/Form1.cs
+++ kurs_valut/Form1.cs
@@ -43,7 +43,60 @@ namespace kurs_valut
             }
             return "";
         }
+        private void kurs() //отношение курсов
+        {
+            string selectedState1 = comboBox1.SelectedItem.ToString();
+            string selectedState2 = comboBox2.SelectedItem.ToString();
+            if (selectedState1 != "RUB" && selectedState2 != "RUB")
+            {
+                string selected = get_box1();
+                string val = get_kurs(selected);
+                c1 = Convert.ToDouble(val); // наше значение 1 колонки
 
+                selected = get_box2();
+                val = get_kurs(selected);
+                c2 = Convert.ToDouble(val); // значение 2 колонки
+
+                string nn = textBox1.Text;
+                double n = Convert.ToDouble(nn);
+
+                textBox4.Text = Convert.ToString((c1 / c2) * n);
+            }
+            else
+            {
+                string selected1 = get_box1();
+                string selected2 = get_box2();
+
+                if (selected1 == "RUB") // если первая колонка рубль
+                {
+                    string val2 = get_kurs(selected2);
+                    c2 = Convert.ToDouble(val2); // значение 2 колонки
+                    textBox4.Text = "1";
+                    string nn = textBox1.Text;
+                    double n = Convert.ToDouble(nn);
+                    textBox1.Text = Convert.ToString(c2);
+                }
+                else if (selected2 == "RUB")
+                {
+                    string val1 = get_kurs(selected1);
+                    c1 = Convert.ToDouble(val1); // значение 2 колонки
+                    textBox1.Text = "1";
+                    string nn = textBox4.Text;
+                    double n = Convert.ToDouble(nn);
+                    textBox4.Text = Convert.ToString(c1);
+                }
+            }
+        }
+        public string get_box1()//выбор comboBox1
+        {
+            string selectedState = comboBox1.SelectedItem.ToString();
+            return selectedState;
+        }
+        public string get_box2()//выбор comboBox2
+        {
+            string selectedState = comboBox2.SelectedItem.ToString();
+            return selectedState;
+        }
         private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
         {
             string selectedState = comboBox2.SelectedItem.ToString();
