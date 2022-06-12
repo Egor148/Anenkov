7ac30920abc3a2f6ad898ad944d69be4ae5fc801
EgorAn <57406043+Egor148@users.noreply.github.com>
2022-06-12 15:28:16 +0700

    добавлен класс валюта

---
+++ kurs_valut/Valute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kurs_valut
+{
+    class Valute
+    {
+        public string[] name = new[] { "USD", "EUR", "RUB", "BYN", "INR", "KZT", "CAD", "CNY", "UZS" };
+        public string id; //номер валюты
+    }
+}
